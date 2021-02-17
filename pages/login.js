785caba1b8245a49645474e000bb7e47a8c5c4fb
Spencer Pugh <spugh@g.ucla.edu>
2021-02-17 00:17:39 -0800

    login with spotify fe

--- pages/login.js
+++ pages/login.js
@@ -9,41 +9,20 @@ const LoginPage = (props) => {
     useEffect(() => {
       fetch('https://musaic-13018.herokuapp.com/login/appdetails')
       .then(response => response.json())
-      .then(data => {
+      .then((data) => {
         const redurl = new URL("https://accounts.spotify.com/authorize");
         redurl.searchParams.append("client_id",data.client_id);
         redurl.searchParams.append("response_type","code");
         redurl.searchParams.append("redirect_uri",data.redirect_uri);
         redurl.searchParams.append("scope",data.scopes);
-        window.location.href = redurl.href}/*this.setState({ client_id:data.client_id, scopes:data.scopes, redirect_uri:data.redirect_uri })*/);
-        //window.location.href="https://google.com";
-      }, []);
+        //return <div>redurl.href</div>;
+        window.location.href = redurl.href;
+        },
+        (error) => {});
+      }, [])
+    return null;
   }
 
-  /*fetch('https://musaic-13018.herokuapp.com/login/appdetails')
-    .then(response => response.json())
-    .then(data => {
-      const redurl = new URL("https://accounts.spotify.com/authorize");
-      redurl.searchParams.append("client_id",data.client_id);
-      redurl.searchParams.append("response_type","code");
-      redurl.searchParams.append("redirect_uri",data.redirect_uri);
-      redurl.searchParams.append("scope",data.scopes);
-      window.location.href = redurl.href}/*this.setState({ client_id:data.client_id, scopes:data.scopes, redirect_uri:data.redirect_uri }));*/
-
-
-  return (
-    <div>
-      <p>Hello!</p>
-      <Link href="/questionnaire">
-        <button type="button" className="btn btn-primary">
-          Log in!
-        </button>
-      </Link>
-    </div>
-  );
 };
 
 export default LoginPage;
-
-//get app details from https://musaic-13018.herokuapp.com/login/appdetails
-//
