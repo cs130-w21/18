b72299510393ed7b586c6906b6e96ce23487c579
Shree Kesava Narayan Prasanna <30460127+keshpr@users.noreply.github.com>
2021-02-22 13:23:23 -0500

    Store moods in DB (#37)
    * Add DB util
    
    * Use strategy pattern to generate moods
    
    * Incorporate DB and mood generator into request handlers
    
    * Add database url to travis yml

--- backend/mood.py
+++ backend/mood.py
@@ -1,29 +1,14 @@
 from flask import Blueprint, request, abort, jsonify, Response, g
-from marshmallow import Schema, fields, validate, exceptions
+from marshmallow import exceptions
 import copy
 import json
 from .auth import extract_credentials
+from .mood_generator import MoodGenerator, CreateOrUpdateMoodStrategy, \
+		GetMoodFromDBStrategy, DeleteMoodFromDBStrategy
 
 mood_api = Blueprint('mood_api', __name__)
 mood_api.before_request(extract_credentials)
 
-# TODO: use non-volatile memory to preserve moods even if server crashes
-mood_cache = {}
-
-# (Create or Update) Mood Schema
-class CreateUpdateMood(Schema):
-	seed_artists = fields.List(fields.String(), required=True, validate=validate.Length(min=1))
-	seed_genres = fields.List(fields.String(), required=True, validate=validate.Length(min=1))
-	seed_tracks = fields.List(fields.String(), required=True, validate=validate.Length(min=1))
-
-	# each is num list [min, max, target]
-	# TODO: check if min < target < max
-	danceability = fields.List(fields.Number(), required=False, validate=validate.Length(equal=3))
-	instrumentalness = fields.List(fields.Number(), required=False, validate=validate.Length(equal=3))
-	popularity = fields.List(fields.Number(), required=False, validate=validate.Length(equal=3))
-	speechiness = fields.List(fields.Number(), required=False, validate=validate.Length(equal=3))
-	valence = fields.List(fields.Number(), required=False, validate=validate.Length(equal=3))
-	energy = fields.List(fields.Number(), required=False, validate=validate.Length(equal=3))
 
 # Create OR update mood (PUT request)
 @mood_api.route("/mood", methods=['PUT'])
@@ -37,15 +22,14 @@ def create_update_custom_mood():
 
 	# Request body validation
 	# Reference: https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-recommendations
+	data = request.data.decode("utf8")
+	generator = MoodGenerator(name, g.user_id, data, CreateOrUpdateMoodStrategy)
 	try:
-		data = CreateUpdateMood().loads(request.data.decode("utf8"))
+		mood = generator.generate()
 	except exceptions.ValidationError as err:
 		abort(422, description="Unprocessable entity: " + str(err.messages))
 
-	user = g.user_id
-	if user not in mood_cache:
-		mood_cache[user] = {}
-	mood_cache[user][name] = data
+	data = mood.params
 
 	print(data)
 	return jsonify(data)
@@ -60,13 +44,11 @@ def delete_custom_mood():
 	if name is None:
 		abort(422, description="Unprocessable entity: missing mood name query string")
 
-	user = g.user_id
-	# Don't bother checking if mood actually exists
-	# Only need to delete if it exists
-	if user in mood_cache and name in mood_cache[user]:
-		mood_to_del = copy.deepcopy(mood_cache[user][name])
-		del mood_cache[user][name]
-		return jsonify(mood_to_del)
+	generator = MoodGenerator(name, g.user_id, None, DeleteMoodFromDBStrategy)
+	mood = generator.generate()
+
+	if not mood is None:
+		return jsonify(mood.params)
 	return Response(status = 200)
 
 # Read mood (GET request)
@@ -79,7 +61,9 @@ def get_custom_mood():
 	if name is None:
 		abort(422, description="Unprocessable entity: missing mood name query string")
 
-	user = g.user_id
-	if user in mood_cache and name in mood_cache[user]:
-		return jsonify(mood_cache[user][name])
+	generator = MoodGenerator(name, g.user_id, None, GetMoodFromDBStrategy)
+	mood = generator.generate()
+
+	if not mood is None:
+		return jsonify(mood.params)
 	return Response(status = 404)
