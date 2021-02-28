48e4cbf0fe84854b3148df20bd9d88f84b67eab8
yuyingchua <ychua@ucla.edu>
2021-02-27 16:18:39 -0800

    Change login function, delete unneeded files

--- pages/index.js
+++ pages/index.js
@@ -92,26 +92,18 @@ export default function HomeController(props) {
     return newPlaylist;
   };
 
-  const loginFunction = async (props) => {
-    if (props.loggedIn) {
-      return <Redirect to="/" />;
-    } else {
-      // useEffect(() => {
-        fetch('https://musaic-13018.herokuapp.com/login/appdetails')
-        .then(response => response.json())
-        .then((data) => {
-          const redurl = new URL("https://accounts.spotify.com/authorize");
-          redurl.searchParams.append("client_id",data.client_id);
-          redurl.searchParams.append("response_type","code");
-          redurl.searchParams.append("redirect_uri",data.redirect_uri);
-          redurl.searchParams.append("scope",data.scopes);
-          window.location.href = redurl.href;
-          },
-          (error) => {});
-        // }, [])
-      return null;
-    }
-    return;
+  const loginFunction = async () => {
+    fetch('https://musaic-13018.herokuapp.com/login/appdetails')
+    .then(response => response.json())
+    .then((data) => {
+      const redurl = new URL("https://accounts.spotify.com/authorize");
+      redurl.searchParams.append("client_id",data.client_id);
+      redurl.searchParams.append("response_type","code");
+      redurl.searchParams.append("redirect_uri",data.redirect_uri);
+      redurl.searchParams.append("scope",data.scopes);
+      window.location.href = redurl.href;
+      },
+      (error) => {});
   };
 
   const logoutFunction = () => {
