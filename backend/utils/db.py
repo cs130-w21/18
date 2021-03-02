e2864bf3cab14b06682e1e36a8602a3e2547a949
Shree Kesava Narayan Prasanna <kesavapr@gmail.com>
2021-03-02 05:07:16 -0500

    Add User schema to DB

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -25,7 +25,14 @@ class DB:
                 "PRIMARY KEY (name, creator_id)"
                 ");"
                 )
+        create_users = (
+                "CREATE TABLE IF NOT EXISTS User ("
+                "user_id varchar PRIMARY KEY,"
+                "moods integer[] DEFAULT array[]::integer[]"
+                ");"
+                )
         self._cursor.execute(create_moods)
+        self._cursor.execute(create_users)
         #TODO: Create Users and Playlists tables
 
     def create_mood(self, name, creator_id, params):
@@ -74,3 +81,52 @@ class DB:
         if row is None:
             return None, None
         return row[0], row[3]
+
+    def create_user(self, user_id):
+        insert = (
+                "INSERT INTO Users (user_id) values (%s);"
+                )
+        try:
+            self._cursor.execute(insert, (user_id,))
+        except psycopg2.Error as e:
+            return False
+        return True
+
+    def get_user_mood_ids(self, user_id):
+        select = (
+                "SELECT * FROM Users WHERE user_id = %s;"
+                )
+        self._cursor.execute(select, (user_id,))
+        row = self._cursor.fetchone()
+        if row is None:
+            return None
+        return row[1]
+
+    def get_user_moods(self, user_id):
+        moods = self.get_user_mood_ids(user_id)
+        if moods is None:
+            return None
+        select = (
+                "SELECT mood_id, name, params FROM Moods WHERE mood_id = ANY(%s);"
+                )
+        self.cursor.execute(select, (moods,))
+        rows = self._cursor.fetch
+        if rows is None:
+            return None
+        return [{'mood_id': row[0], 'mood_name': row[1], **row[2]} for row in rows]
+
+    def add_mood_for_user(self, user_id, mood_id):
+        user_moods = self.get_user_mood_ids(user_id)
+        if user_moods is None:
+            return False
+        if mood_id in user_moods:
+            return False
+        update = (
+                "UPDATE Users SET moods = array_append(moods, %s) "
+                "WHERE user_id = %s;"
+                )
+        try:
+            self.cursor.execute(update, (mood_id, user_id))
+        except psycopg2.Error as e:
+            return False
+        return True
