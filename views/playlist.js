fbfe43ad2e40623c9d7a51c84a947689566a4833
willob99 <willobrien00@gmail.com>
2021-03-02 18:24:55 -0800

    Parameterize playlist display page

--- views/playlist.js
+++ views/playlist.js
@@ -1,16 +1,32 @@
 import React from "react";
 import PageHead from "../components/head";
 import styles from "../styles/playlist.module.css";
+import { useRouter } from "next/router";
 
+const Playlist = () => {
+  const router = useRouter();
+  const { playlist } = router.query;
 
-const Playlist = (props) => {
-  return (<>
-    <PageHead/>
-    <div className={`${styles.playlistContainer}`}>
-      <div className={`${styles.embed} ${styles.playlistText}`}><p>Here's your custom playlist!</p></div>
-      <div className={`${styles.embed}`}><iframe src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>
-    </div>
-  </>);
+  return (
+    <>
+      <PageHead />
+      <div className={`${styles.playlistContainer}`}>
+        <div className={`${styles.embed} ${styles.playlistText}`}>
+          <p>Here's your custom playlist!</p>
+        </div>
+        <div className={`${styles.embed}`}>
+          <iframe
+            src={`https://open.spotify.com/embed/playlist/${playlist}`}
+            width="300"
+            height="380"
+            frameBorder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+        </div>
+      </div>
+    </>
+  );
 };
-  
+
 export default Playlist;
