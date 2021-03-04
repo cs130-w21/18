1d96e67808465b79d5b94ca9c9914fab886e59e2
qwerhacks <arjun.subramonian@gmail.com>
2021-03-04 08:04:44 -0800

    fixed indentation errors

--- backend/playlist_generator.py
+++ backend/playlist_generator.py
@@ -2,26 +2,26 @@ from .utils.db import DB
 
 # TODO: complete stub code
 class PlaylistGenerator:
-    def __init__(self, mood_id, creator_id, params, strategy):
-    	self.mood_id = mood_id
-      self.creator_id = creator_id
-      self.params = params
-      self.strategy = strategy
+	def __init__(self, mood_id, creator_id, params, strategy):
+		self.mood_id = mood_id
+		self.creator_id = creator_id
+		self.params = params
+		self.strategy = strategy
 
-    def set_strategy(self, strategy):
-        self.strategy = strategy
+		def set_strategy(self, strategy):
+			self.strategy = strategy
 
-    def generate(self):
-    	return self.strategy(self.mood_id, self.creator_id, self.params).generate()
+		def generate(self):
+			return self.strategy(self.mood_id, self.creator_id, self.params).generate()
 
 class GenerationStrategy:
-    def __init__(self, mood_id, creator_id, params):
-    		self.mood_id = mood_id
-        self.creator_id = creator_id
-        self.params = params
-    
-    def generate(self):
-        raise NotImplementedError
+	def __init__(self, mood_id, creator_id, params):
+		self.mood_id = mood_id
+		self.creator_id = creator_id
+		self.params = params
+
+	def generate(self):
+		raise NotImplementedError
 
 class GetPlaylistsFromDBStrategy(GenerationStrategy):
 	def generate(self):
