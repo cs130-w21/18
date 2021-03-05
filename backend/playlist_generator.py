4aeb0f3e520f6428bb00fb81940fd216c079fb27
Shree Kesava Narayan Prasanna <30460127+keshpr@users.noreply.github.com>
2021-03-05 00:07:13 -0500

    Add Playlist Schema (#69)

--- backend/playlist_generator.py
+++ backend/playlist_generator.py
@@ -1,28 +1,52 @@
 from .utils.db import DB
 
+class Playlist:
+	def __init__(self, creator_id, mood_id, idx, uri):
+		self.creator_id = creator_id
+		self.mood_id = mood_id
+		self.idx = idx
+		self.uri = uri
+	
+	def to_dict(self):
+		return {'mood_id': self.mood_id, 'idx': self.idx, 'uri': self.uri}
+
 # TODO: complete stub code
 class PlaylistGenerator:
-	def __init__(self, mood_id, creator_id, params, strategy):
+	def __init__(self, mood_id, creator_id, uri, strategy):
 		self.mood_id = mood_id
 		self.creator_id = creator_id
-		self.params = params
+		self.uri = uri
 		self.strategy = strategy
 
-		def set_strategy(self, strategy):
-			self.strategy = strategy
+	def set_strategy(self, strategy):
+		self.strategy = strategy
 
-		def generate(self):
-			return self.strategy(self.mood_id, self.creator_id, self.params).generate()
+	def generate(self):
+		return self.strategy(self.mood_id, self.creator_id, self.uri).generate()
 
 class GenerationStrategy:
-	def __init__(self, mood_id, creator_id, params):
+	def __init__(self, mood_id, creator_id, uri):
 		self.mood_id = mood_id
 		self.creator_id = creator_id
-		self.params = params
+		self.uri = uri
 
 	def generate(self):
 		raise NotImplementedError
 
 class GetPlaylistsFromDBStrategy(GenerationStrategy):
 	def generate(self):
-		return None
+		rows = None
+		with DB() as db:
+			rows = db.get_mood_playlists(self.creator_id, self.mood_id)
+		if rows is None:
+			return None
+		return [Playlist(row[0], row[1], row[2], row[3]) for row in rows]
+
+class StorePlaylistInDBStrategy(GenerationStrategy):
+	def generate(self):
+		idx = None
+		with DB() as db:
+			idx = db.create_playlist_on_mood(self.creator_id, self.mood_id, self.uri)
+		if idx is None:
+			return None
+		return Playlist(self.creator_id, self.mood_id, idx, self.uri)
