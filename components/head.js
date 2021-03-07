bb07eaec4097266f82e088bd9ba8785c600725d5
yuyingchua <ychua@ucla.edu>
2021-03-07 00:55:29 -0800

    Document head.js

--- components/head.js
+++ components/head.js
@@ -1,12 +1,19 @@
-import Head from 'next/head';
-import React from 'react';
+import Head from "next/head";
+import React from "react";
 
+/**
+ * Component to standardize Page Headers
+ * @class PageHead
+ */
 const PageHead = () => (
   <Head>
     <title>Custom Mood-Based Playlists</title>
     <link rel="icon" href="/favicon.ico" />
-    <link rel="preconnect" href="https://fonts.gstatic.com"/>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+      rel="stylesheet"
+    />
   </Head>
 );
 
