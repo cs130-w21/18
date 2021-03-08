51810a8ae16a9788c1f49ad7f4bfe0b3a2332d6d
Shree Kesava Narayan Prasanna <kesavapr@gmail.com>
2021-03-07 19:07:31 -0500

    debugging

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -1,8 +1,12 @@
 import json
 import flask
-import JWT
+import sys
+import os
 import pytest
-from .test_mood import test_create_mood, get_data
+from test_mood import test_create_mood, test_delete_mood, get_data
+sys.path.insert(0, os.path.abspath('.'))
+from backend.utils.jwt import JWT
+from backend.utils.db import DB
 
 # Description: tests getting all moods created by user
 # Expected input: Authorization header with JWT
@@ -13,18 +17,24 @@ from .test_mood import test_create_mood, get_data
 # If any expected outcomes are not satisfied, then test fails.
 # If all expected outcomes are satisified, then test succeeds.
 def test_get_user_moods(client, app):
+	with DB() as db:
+		db.create_or_update_user('fake', 'fake token')
+
+	test_delete_mood(client, app)
 	test_create_mood(client, app)
 
 	_, data, headers = get_data()
 	USER_ENDPOINT = '/api/v1/user/moods'
 
-	json_resp = client.get(USER_ENDPOINT, headers=json.dumps(headers)).json()
-  assert(len(json_resp) == 1)
-  json_resp = json_resp[0]
-  mood = dict((k, json_resp[k]) for k in json_resp if k != 'mood_id')
-  assert(mood == data)
+	json_resp = client.get(USER_ENDPOINT, headers=headers).json
+	assert(len(json_resp) == 2)
+	created_moods = json_resp['created_moods']
+	assert(len(created_moods) == 1)
+	json_resp = created_moods[0]['params']
+	mood = dict((k, json_resp[k]) for k in json_resp if k != 'mood_id')
+	assert(mood == data)
 
-  test_delete_mood(client, app)
+	test_delete_mood(client, app)
 
 # Description: tests adding external mood to user's collection of moods
 # Expected input: Authorization header with JWT, mood_id query string
@@ -36,8 +46,8 @@ def test_add_external_mood(client, app):
 	_, data, headers = get_data()
 	USER_ENDPOINT = '/api/v1/user/external_mood?mood_id=1'
 
-	resp = client.put(USER_ENDPOINT, data=json.dumps(data), headers=json.dumps(headers))
-	assert(resp.status == 200)
+	resp = client.put(USER_ENDPOINT, data=json.dumps(data), headers=headers)
+	assert(resp.status_code == 200)
 
 # Description: tests adding external mood with invalid mood ID to user's collection
 # Expected input: Authorization header with JWT, mood_id query string
@@ -49,6 +59,6 @@ def test_add_external_mood_fail(client, app):
 	_, data, headers = get_data()
 	USER_ENDPOINT = '/api/v1/user/external_mood?mood_id=c'
 
-	resp = client.put(USER_ENDPOINT, data=json.dumps(data), headers=json.dumps(headers))
-	assert(resp.status == 422)
+	resp = client.put(USER_ENDPOINT, data=json.dumps(data), headers=headers)
+	assert(resp.status_code == 422)
 
